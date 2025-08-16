6921597e5e0fd0d7c400f2c8411a582f2bc301fd
akhila-sys <akhila.oct2018@gmail.com>
2025-08-16 16:56:00 +0530

    Add files via upload

---
+++ EmployeeCrud/src/main/java/com/example/EmployeeCrud/controller/EmployeeController.java
@@ -0,0 +1,49 @@
+package com.example.EmployeeCrud.controller;
+
+import com.example.EmployeeCrud.model.Employee;
+import com.example.EmployeeCrud.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+        return ResponseEntity.ok(employee);
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+        employee.setName(employeeDetails.getName());
+        employee.setRole(employeeDetails.getRole());
+        final Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+        employeeRepository.delete(employee);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ EmployeeCrud/target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee (name, role) VALUES ('John Doe', 'Developer');
+INSERT INTO employee (name, role) VALUES ('Jane Smith', 'Manager');
+INSERT INTO employee (name,role) VALUES ('akhila','Developer');
+INSERT INTO employee (name,role) VALUES ('sireesha','developer');
